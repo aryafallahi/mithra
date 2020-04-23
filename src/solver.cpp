f23e0ef979a8567f0c4db36e7e6979f2171a0d51
Arnau Albà <albajacas_a@psi.ch>
2020-04-23 12:45:58 +0200

    automatic bunch time step if set to 0

--- src/solver.cpp
+++ src/solver.cpp
@@ -235,7 +235,10 @@ namespace MITHRA
     bunch_.timeStep_			/= gamma_;
 
     /* Adjust the given bunch time step according to the given field time step.				*/
-    bunch_.timeStep_ 			 = mesh_.timeStep_ / ceil(mesh_.timeStep_ / bunch_.timeStep_);
+    if (bunch_.timeStep_ == 0)
+      bunch_.timeStep_ 			= mesh_.timeStep_ ;
+    else
+      bunch_.timeStep_ 			 = mesh_.timeStep_ / ceil(mesh_.timeStep_ / bunch_.timeStep_);
     nUpdateBunch_    			 = mesh_.timeStep_ / bunch_.timeStep_;
     printmessage(std::string(__FILE__), __LINE__, std::string("Time step for the bunch update is set to " + stringify(bunch_.timeStep_ * gamma_) ) );
 
